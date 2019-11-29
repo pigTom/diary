61ad4f7a4f13f45c09a3e6fc7b27d278bc818949
pigtom <13393895196@163.com>
2019-11-29 11:03:45 +0800

    去掉Test中错误的方法

--- src/test/java/com/pigtom/diary/SysterUser/web/FileDownloadControllerTest.java
+++ src/test/java/com/pigtom/diary/SysterUser/web/FileDownloadControllerTest.java
@@ -1,23 +1,11 @@
 package com.pigtom.diary.SysterUser.web;
 
 import com.pigtom.diary.controller.FileDownloadController;
-import com.pigtom.diary.model.bean.SystemUser;
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.io.File;
-import java.net.URI;
-import java.nio.file.Path;
-import java.util.Map;
-import java.util.Properties;
 
 /**
  * @author tangdunhong
@@ -34,15 +22,17 @@ public class FileDownloadControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(new FileDownloadController()).build();  //构造MockMvc
     }
 
-    @Test
-    public void downLoad() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v0.1/file/a.pdf")).andReturn();
-    }
 
-    @Test
-    public void testPath() {
-        System.out.println(System.getProperty("user.dir"));
-        String file = System.getProperty("user.dir");
-        file = file.concat("/src/main/resources/jls8.pdf");
-    }
+//    @Test
+//    public void testPath() throws Exception{
+//        System.out.println(System.getProperty("user.dir"));
+//        String file = System.getProperty("user.dir");
+//        file = file.concat("/application.yml");
+//        FileInputStream inputStream = new FileInputStream(new File(file));
+//        BufferedInputStream bi = new BufferedInputStream(inputStream);
+//        FileReader rf = new FileReader(file);
+//        char[] chars = new char[2049];
+//        int i = rf.read(chars);
+//        System.out.println(chars);
+//    }
 }
