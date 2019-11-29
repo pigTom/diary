134b6a2b613dd6d956d753851586e7674857e5ae
pigtom <13393895196@163.com>
2019-11-29 13:33:13 +0800

    去掉spring config

--- src/main/java/com/pigtom/diary/App.java
+++ src/main/java/com/pigtom/diary/App.java
@@ -1,11 +1,7 @@
 package com.pigtom.diary;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author tangdunhong
@@ -15,28 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 
 @SpringBootApplication
-@RestController
-@RefreshScope
 public class App {
-    public static void main1(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
-
     public static void main(String[] args) {
-        SpringApplication.run(App.class);
-    }
-
-
-    @Value("${server.port}")
-    private int port;
-
-//    public void setPort(int port){
-//        this.port=port;
-//    }
-
-    @RequestMapping("/port")
-    public int port(){
-        return port;
+        SpringApplication.run(App.class, args);
     }
 }
